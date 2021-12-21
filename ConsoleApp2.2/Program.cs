ddd50351c3bf9fc8dc6e0c5c6e1dca6227ab1b48
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-21 19:32:11 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double x, y, R;
+            Console.WriteLine("Введите x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите R:");
+            R = Convert.ToDouble(Console.ReadLine());
+            if (Math.Sqrt(x * x + y * y) <= R) Console.WriteLine("Точка ({0};{1}) входит в окружность с радиусом {2}", x, y, R);
+            else Console.WriteLine("Точка ({0};{1}) не входит в окружность с радиусом {2}", x, y, R);
+            Console.ReadKey(true);
+        }
+    
+    }
+}
